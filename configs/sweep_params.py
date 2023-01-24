ec767075db27be571d3b2cbe46c5253b2d38feaa
Emadeldeen Eldele <37911596+emadeldeen24@users.noreply.github.com>
2023-01-24 12:24:36 +0800

    Update sweep_params.py

--- configs/sweep_params.py
+++ configs/sweep_params.py
@@ -3,6 +3,7 @@ sweep_train_hparams = {}
 sweep_alg_hparams = {
         'CoTMix': {
             'learning_rate':            {'values': [5e-3, 3e-3, 1e-3, 7e-3]},
+            'temporal_shift':           {'values': [5, 10, 15, 20, 30, 50]},
             'src_cls_weight':           {'distribution': 'uniform', 'min': 1e-1, 'max': 1},
             'mix_ratio':                {'distribution': 'uniform', 'min': 0.5, 'max': 0.99},
             'src_supCon_weight':        {'distribution': 'uniform', 'min': 1e-3, 'max': 1},
